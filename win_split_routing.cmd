ca9f606e3c33f63851e9551b3144bda0b3630658
colemar <colemarc@gmail.com>
2025-06-05 16:39:57 +0200

    Update win_split_routing.cmd

--- win_split_routing.cmd
+++ win_split_routing.cmd
@@ -13,11 +13,16 @@ DATA#end
 
 :: The index of the interface connected to the global internet (see: netsh interface ipv4 show interface)
 :: Most likely the WiFi interface
-set INTERNET_IF_IDX=13
+set INTERNET_IF_IDX=12
 :: The index of the interface connected to the LAN
-set LAN_IF_IDX=8
+set LAN_IF_IDX=7
 
 setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+
+:: Capture 0x1B (escape) into variable ESC
+for /f "delims=" %%E in ('forfiles /p "%~dp0." /m "%~nx0" /c "cmd /c echo(0x1B"') do (
+  set "ESC=%%E["
+)
 
 if "%1"=="on" (
   set ACTION=on
@@ -25,7 +30,7 @@ if "%1"=="on" (
   set ACTION=off
 ) else if "%1"=="show" (
   call :SHOWIF
-  call :SHOWPR
+  call :SHOWROUTES
   pause
   exit /b
 ) else (
@@ -43,7 +48,7 @@ call :GETIFPARM
 if "%INTERNET_IF_NAME%"=="" (
   echo Cannot find interface %INTERNET_IF_IDX%
   call :SHOWIF
-  exit /b
+  exit /b 1
 )
 
 if %ACTION%==off (
@@ -52,36 +57,54 @@ if %ACTION%==off (
   exit /b
 )
 
-if %LAN_IF_METRIC% LEQ 5 (
-  echo Cannot find a suitable LAN_IF_METRIC for interface %LAN_IF_IDX%
+if %LAN_IF_METRIC%=="" (
+  echo Cannot find metric for interface %LAN_IF_IDX%
   call :SHOWIF
-  exit /b
+  exit /b 1
 )
 
 call :GETDEFGW
-if not defined DEFGATEWAY (
+if not defined LAN_DEFGATEWAY (
   echo Cannot find default gateway of interface %LAN_IF_IDX% "%LAN_IF_NAME%"
-  exit /b
+  exit /b 1
+)
+if not defined INTERNET_DEFGATEWAY (
+  echo Cannot find default gateway of interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%"
+  exit /b 1
 )
 
 echo Current metric of interface %LAN_IF_IDX% "%LAN_IF_NAME%" is %LAN_IF_METRIC%
 
-:: Set TARGET_INTERNET_METRIC to 5 less than LAN_IF_METRIC (instead of automatic) as to prioritize INTERNET_IF_IDX
-set /A TARGET_INTERNET_METRIC=LAN_IF_METRIC-5
 echo.
-echo Setting metric=%TARGET_INTERNET_METRIC% for interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%"
-netsh interface ipv4 set interface interface=%INTERNET_IF_IDX% metric=%TARGET_INTERNET_METRIC%
+echo Setting metric=1 for interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%"
+netsh interface ipv4 set interface interface=%INTERNET_IF_IDX% metric=1
 call :SHOWIF
 
-echo Current default gateway of interface %LAN_IF_IDX% "%LAN_IF_NAME%" is %DEFGATEWAY%
+call :GETRM
+if not defined LAN_R_METRIC (
+  echo Cannot find default route metric of interface %LAN_IF_IDX% "%LAN_IF_NAME%"
+  exit /b 1
+)
+if not defined INTERNET_R_METRIC (
+  echo Cannot find default route metric of interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%"
+  exit /b 1
+)
+
+echo Current default gateway of interface %LAN_IF_IDX% "%LAN_IF_NAME%" is %LAN_DEFGATEWAY%
+echo Current default gateway of interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%" is %INTERNET_DEFGATEWAY%
 echo.
 
-call :SETPR %DEFGATEWAY%
+call :SETPR %LAN_DEFGATEWAY%
 
-call :SHOWPR
+call :SHOWROUTES
+
+if %INTERNET_R_METRIC% GEQ %LAN_R_METRIC% (
+  echo The default route metric for interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%" ^(%INTERNET_R_METRIC%^) is still greater than or equal to that of interface %LAN_IF_IDX% "%LAN_IF_NAME%" ^(%LAN_R_METRIC%^)
+  exit /b 1
+)
 
 pause
-goto :EOF
+exit /b 0
 
 :: ===========================================================================
 :: Subroutines
@@ -89,14 +112,14 @@ goto :EOF
 :CHECKADMIN
 :: Check administrative privileges
 fsutil dirty query %systemdrive% > nul
-if %ERRORLEVEL% LSS 1 goto :EOF
+if %ERRORLEVEL% LSS 1 exit /b 0
 echo  This script must run with "elevated privileges", therefore:
 echo    right-click and choose "Run as administrator",
 echo    or run it from an elevated console,
 echo    or run it from gsudo.
 echo.
 pause
-exit /b
+exit /b 0
 
 :GETIFPARM
 :: Find metric and name of interfaces
@@ -110,7 +133,7 @@ for /f "tokens=1,2,3,4*" %%a in ('netsh interface ipv4 show interface') do (
     set INTERNET_IF_NAME=%%e
   )
 )
-goto :EOF
+exit /b 0
 
 :TURNOFF
 echo Re-enabling automatic metric for interface %INTERNET_IF_IDX% "%INTERNET_IF_NAME%"
@@ -135,19 +158,27 @@ for /f "tokens=1,2 delims=#" %%a in (%THISFILE%) do (
   )
 )
 :ROUTESREMOVED
-call :SHOWPR
-goto :EOF
+call :SHOWROUTES
+exit /b 0
 
 :GETDEFGW
-:: Gets the default gateway of the interface LAN_IF_IDX
-set DEFGATEWAY=
+:: Gets the default gateway of the interfaces
+set LAN_DEFGATEWAY=
+set INTERNET_DEFGATEWAY=
+set count=0
 for /f "tokens=1,2,3,4,5*" %%a in ('netsh interface ipv4 show route') do (
-  if %%d%%e==0.0.0.0/0%LAN_IF_IDX% (
-    set DEFGATEWAY=%%f
-    goto :EOF
+  if %%d==0.0.0.0/0 (
+    if %%e==%LAN_IF_IDX% (
+      set LAN_DEFGATEWAY=%%f
+      set /A count+=1
+    ) else if %%e==%INTERNET_IF_IDX% (
+      set INTERNET_DEFGATEWAY=%%f
+      set /A count+=1
+    )
   )
+  if !count! EQU 2 exit /b 0
 )
-goto :EOF
+exit /b 0
 
 :SETPR
 :: Get subnets from this file and set permanent local routes 
@@ -157,11 +188,11 @@ set status=0
 for /f "tokens=1,2 delims=#" %%a in (%THISFILE%) do (
   if %%a==DATA (
     if !status!==1 (
-      goto :EOF
+      exit /b 0
     ) else if %%b==subnets (
       set status=1
     ) else if %%b==end (
-      goto :EOF
+      exit /b 0
     )
   ) else (
     if !status!==1 (
@@ -170,29 +201,68 @@ for /f "tokens=1,2 delims=#" %%a in (%THISFILE%) do (
     )
   )
 )
-goto :EOF
+exit /b 0
 
-:SHOWPR
-:: Show permanent routes
+:SHOWROUTES
+:: Show routes
 set count=0
-for /f "tokens=1 delims=" %%a in ('route -4 print') do (
+set r=0
+for /f "delims=" %%a in ('route -4 print') do (
   set a=%%a
   set a=!a:~0,1!
   if "!a!"=="=" set /A count+=1
-  if !count! EQU 4 echo %%a
+  if !count! EQU 3 (
+    set /A r+=1
+    set a=%%a
+    set a=!a: =!
+    set a=!a:~0,7!
+    set print=1
+    if not !r! LEQ 3 if not "!a!"=="0.0.0.0" set print=0
+    if !print!==1 echo %%a
+  ) else if !count! EQU 4 echo %%a
 )
 echo.
-goto :EOF
+exit /b 0
 
 :SHOWIF
 :: Show interfaces
-netsh interface ipv4 show interface
+for /f "delims=" %%a in ('netsh interface ipv4 show interface') do (
+  set a=%%a
+  set a=!a:~0,3!
+  set a=!a: =!
+  if !a! EQU %LAN_IF_IDX% (
+    echo %ESC%93;40m%%a%ESC%0m
+  ) else if !a! EQU %INTERNET_IF_IDX% (
+    echo %ESC%92;40m%%a%ESC%0m
+  ) else (
+    echo %%a
+  )
+)
 set count=0
-for /f "tokens=1 delims=" %%a in ('route -4 print') do (
+for /f "delims=" %%a in ('route -4 print') do (
   set a=%%a
   set a=!a:~0,1!
   if "!a!"=="=" set /A count+=1
   if !count! EQU 1 echo %%a
 )
 echo.
-goto :EOF
+exit /b 0
+
+:GETRM
+:: Get default route metrics
+set LAN_R_METRIC=
+set INTERNET_R_METRIC=
+set count=0
+for /f "tokens=1,2,3,4,5" %%a in ('route -4 print') do (
+  if %%a==0.0.0.0 (
+    if %%c==%LAN_DEFGATEWAY% (
+      set LAN_R_METRIC=%%e
+      set /A count+=1
+    ) else if %%c==%INTERNET_DEFGATEWAY% (
+      set INTERNET_R_METRIC=%%e
+      set /A count+=1
+    )
+  )
+  if !count! EQU 2 exit /b 0
+)
+exit /b 0
